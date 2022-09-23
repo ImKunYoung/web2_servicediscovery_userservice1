377e9672d9598058827ae8819cf18614909ac861
ImKunYoung <lim8667@gmail.com>
2022-09-23 18:45:30 +0900

    init commit

---
+++ src/main/java/com/example/web2_userservice1/Web2Userservice1Application.java
@@ -0,0 +1,13 @@
+package com.example.web2_userservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2Userservice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web2Userservice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web2_userservice1/Web2Userservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2_userservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2Userservice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
