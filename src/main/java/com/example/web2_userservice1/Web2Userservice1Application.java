28238e6163a2a02a6ff9ca0b293137fb38ac9a93
ImKunYoung <lim8667@gmail.com>
2022-09-23 18:49:08 +0900

    마이크로서비스 세팅

--- src/main/java/com/example/web2_userservice1/Web2Userservice1Application.java
+++ src/main/java/com/example/web2_userservice1/Web2Userservice1Application.java
@@ -2,8 +2,11 @@ package com.example.web2_userservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Web2Userservice1Application {
 
     public static void main(String[] args) {
